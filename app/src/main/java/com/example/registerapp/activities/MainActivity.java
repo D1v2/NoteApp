23d0b8345a68f4177abeda70b81f0fe5a53737dc
Devki Nandan Pandey <dnpanday90@gmail.com>
2020-08-19 12:04:19 +0530

    SearchText

--- app/src/main/java/com/example/registerapp/activities/MainActivity.java
+++ app/src/main/java/com/example/registerapp/activities/MainActivity.java
@@ -9,7 +9,10 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.example.registerapp.R;
@@ -24,51 +27,71 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements NotesListeners {
 
-    public static final int REQUEST_CODE_ADD_NOTE=1;
-    public static final int REQUEST_CODE_UPDATE_NOTES=2;
-    public static final int REQUEST_CODE_SHOW_NOTE=3;
+    public static final int REQUEST_CODE_ADD_NOTE = 1;
+    public static final int REQUEST_CODE_UPDATE_NOTES = 2;
+    public static final int REQUEST_CODE_SHOW_NOTE = 3;
     private RecyclerView notesRecyclerView;
-    private List<Note>noteList;
+    private List<Note> noteList;
     private NotesAdapter notesAdapter;
 
-    private int noteClickedPosition=-1;
+    private int noteClickedPosition = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView imageAddNoteMain=findViewById(R.id.imageaddmain);
+        ImageView imageAddNoteMain = findViewById(R.id.imageaddmain);
         imageAddNoteMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivityForResult(
-                        new Intent(getApplicationContext(), CreateNoteActivity.class),REQUEST_CODE_ADD_NOTE
+                        new Intent(getApplicationContext(), CreateNoteActivity.class), REQUEST_CODE_ADD_NOTE
                 );
             }
         });
 
-        notesRecyclerView=findViewById(R.id.noterecyclerview);
-        notesRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(1,StaggeredGridLayoutManager.VERTICAL));
+        notesRecyclerView = findViewById(R.id.noterecyclerview);
+        notesRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL));
 
-        noteList=new ArrayList<>();
-        notesAdapter=new NotesAdapter(noteList,this);
+        noteList = new ArrayList<>();
+        notesAdapter = new NotesAdapter(noteList, this);
         notesRecyclerView.setAdapter(notesAdapter);
 
-        getNotes(REQUEST_CODE_SHOW_NOTE,false);
+        getNotes(REQUEST_CODE_SHOW_NOTE, false);
+
+        EditText inputSearch = findViewById(R.id.inputsearch);
+        inputSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                notesAdapter.CancleTimer();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (noteList.size() != 0) {
+                    notesAdapter.searchNote(s.toString());
+                }
+            }
+        });
     }
 
     @Override
     public void onNoteClicked(Note note, int position) {
-        noteClickedPosition=position;
-        Intent intent=new Intent(getApplicationContext(),CreateNoteActivity.class);
-        intent.putExtra("isViewOrUpdate",true);
-        intent.putExtra("note",note);
-        startActivityForResult(intent,REQUEST_CODE_UPDATE_NOTES);
+        noteClickedPosition = position;
+        Intent intent = new Intent(getApplicationContext(), CreateNoteActivity.class);
+        intent.putExtra("isViewOrUpdate", true);
+        intent.putExtra("note", note);
+        startActivityForResult(intent, REQUEST_CODE_UPDATE_NOTES);
     }
 
-    private void getNotes(final int requestCode,final boolean isNoteDeleted){
+    private void getNotes(final int requestCode, final boolean isNoteDeleted) {
         @SuppressLint("StaticFieldLeak")
-        class GetNotesTask extends AsyncTask<Void,Void, List<Note>>{
+        class GetNotesTask extends AsyncTask<Void, Void, List<Note>> {
             @Override
             protected List<Note> doInBackground(Void... voids) {
                 return NotesDatabase.getDatabase(getApplicationContext()).noteDao().getAllNotes();
@@ -77,21 +100,19 @@ public class MainActivity extends AppCompatActivity implements NotesListeners {
             @Override
             protected void onPostExecute(List<Note> notes) {
                 super.onPostExecute(notes);
-                if(requestCode == REQUEST_CODE_SHOW_NOTE){
+                if (requestCode == REQUEST_CODE_SHOW_NOTE) {
                     noteList.addAll(notes);
                     notesAdapter.notifyDataSetChanged();
-                }
-                else if(requestCode==REQUEST_CODE_ADD_NOTE){
-                    noteList.add(0,notes.get(0));
+                } else if (requestCode == REQUEST_CODE_ADD_NOTE) {
+                    noteList.add(0, notes.get(0));
                     notesAdapter.notifyItemInserted(0);
                     notesRecyclerView.smoothScrollToPosition(0);
-                }
-                else if(requestCode==REQUEST_CODE_UPDATE_NOTES){
+                } else if (requestCode == REQUEST_CODE_UPDATE_NOTES) {
                     noteList.remove(noteClickedPosition);
-                    if(isNoteDeleted){
+                    if (isNoteDeleted) {
                         notesAdapter.notifyItemRemoved(noteClickedPosition);
-                    }else {
-                        noteList.add(noteClickedPosition,notes.get(noteClickedPosition));
+                    } else {
+                        noteList.add(noteClickedPosition, notes.get(noteClickedPosition));
                         notesAdapter.notifyItemChanged(noteClickedPosition);
                     }
                 }
@@ -103,12 +124,11 @@ public class MainActivity extends AppCompatActivity implements NotesListeners {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if((requestCode == REQUEST_CODE_ADD_NOTE) && resultCode ==  RESULT_OK){
-            getNotes(REQUEST_CODE_ADD_NOTE,false);
-        }
-        else if(requestCode==REQUEST_CODE_UPDATE_NOTES && resultCode==RESULT_OK){
-            if(data!=null){
-                getNotes(REQUEST_CODE_UPDATE_NOTES,data.getBooleanExtra("isNoteDeleted",false));
+        if ((requestCode == REQUEST_CODE_ADD_NOTE) && resultCode == RESULT_OK) {
+            getNotes(REQUEST_CODE_ADD_NOTE, false);
+        } else if (requestCode == REQUEST_CODE_UPDATE_NOTES && resultCode == RESULT_OK) {
+            if (data != null) {
+                getNotes(REQUEST_CODE_UPDATE_NOTES, data.getBooleanExtra("isNoteDeleted", false));
             }
         }
     }
--- app/src/main/java/com/example/registerapp/adapters/NotesAdapter.java
+++ app/src/main/java/com/example/registerapp/adapters/NotesAdapter.java
@@ -3,6 +3,8 @@ package com.example.registerapp.adapters;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,15 +19,22 @@ import com.example.registerapp.entities.Note;
 import com.example.registerapp.listeners.NotesListeners;
 import com.makeramen.roundedimageview.RoundedImageView;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NoteViewHolder>{
     private List<Note>notes;
 
     private NotesListeners notesListeners;
+    private Timer timer;
+    private List<Note>notesSource;
+
     public NotesAdapter(List<Note> notes,NotesListeners notesListeners) {
         this.notes = notes;
         this.notesListeners=notesListeners;
+        notesSource=notes;
     }
 
     @NonNull
@@ -97,6 +106,41 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NoteViewHold
             else {
                 imageNote.setVisibility(View.GONE);
             }
+        }
+    }
+
+    public void searchNote(final String searchKeyword){
+        timer=new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if(searchKeyword.trim().isEmpty()){
+                    notes=notesSource;
+                }
+                else {
+                    ArrayList<Note>temp=new ArrayList<>();
+                    for(Note note : notesSource){
+                        if(note.getTitle().toLowerCase().contains(searchKeyword.toLowerCase())
+                           ||note.getSubtitle().toLowerCase().contains(searchKeyword.toLowerCase())
+                           ||note.getNoteText().toLowerCase().contains(searchKeyword.toLowerCase())){
+                            temp.add(note);
+                        }
+                    }
+                    notes=temp;
+                }
+                new Handler(Looper.getMainLooper()).post(new Runnable() {
+                    @Override
+                    public void run() {
+                        notifyDataSetChanged();
+                    }
+                });
+            }
+        },500);
+    }
+
+    public void CancleTimer(){
+        if(timer!=null){
+            timer.cancel();
         }
     }
 }
